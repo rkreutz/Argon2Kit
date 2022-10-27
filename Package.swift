37f84d7170be993176e568e837ad86c9c8a6edbb
Rodrigo Kreutz <8869678+rkreutz@users.noreply.github.com>
2022-10-28 00:41:25 +0100

    Importing argon2 as submodule

--- Package.swift
+++ Package.swift
@@ -10,13 +10,42 @@ let package = Package(
             name: "Argon2Kit",
             targets: ["Argon2Kit"]),
     ],
-    dependencies: [
-        .package(name: "argon2", url: "https://github.com/P-H-C/phc-winner-argon2", revision: "f57e61e19229e23c4445b85494dbf7c07de721cb")
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "Argon2Kit",
             dependencies: ["argon2"]),
+        .target(
+            name: "argon2",
+            exclude: [
+                "kats",
+                "vs2015",
+                "latex",
+                "libargon2.pc.in",
+                "export.sh",
+                "appveyor.yml",
+                "Argon2.sln",
+                "argon2-specs.pdf",
+                "CHANGELOG.md",
+                "LICENSE",
+                "Makefile",
+                "man",
+                "README.md",
+                "src/bench.c",
+                "src/genkat.c",
+                "src/opt.c",
+                "src/run.c",
+                "src/test.c",
+            ],
+            sources: [
+                "src/blake2/blake2b.c",
+                "src/argon2.c",
+                "src/core.c",
+                "src/encoding.c",
+                "src/ref.c",
+                "src/thread.c"
+            ]
+        ),
         .testTarget(
             name: "Argon2KitTests",
             dependencies: ["Argon2Kit"]),
